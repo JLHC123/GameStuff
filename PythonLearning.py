18a975efc33b99f70e6208bf4c026aea2898f02d
JLHC123 <jlhc3751@gmail.com>
2025-12-17 18:34:35 -0800

    add map display

--- PythonLearning.py
+++ PythonLearning.py
@@ -1,4 +1,5 @@
 print("Game Start")
+# generic maze
 maze = [
     [1, 1, 3, 1, 1],
     [1, 1, 0, 0, 1],
@@ -8,18 +9,36 @@ maze = [
 ]
 
 player_position = None
+# finding our player start position
 for y, row in enumerate(maze):
     for x, cell in enumerate(row):
         if cell == 2:
             player_position = (x, y)
             break
+    if player_position is not None:
+        break
         
 print(player_position)
-player_position_x = player_position[0]
-player_position_y = player_position[1]
-
+player_position_x, player_position_y = player_position
 isInput = True
+
 while (isInput):
+    # map display
+    for y, row in enumerate(maze):
+        cells = []
+        for x, cell in enumerate(row):
+            if (x, y) == player_position:
+                cells.append("P")
+            elif cell == 1:
+                cells.append("#")
+            elif cell == 0:
+                cells.append(".")
+            elif cell == 2:
+                cells.append("S")
+            elif cell == 3:
+                cells.append("E")
+        print(" ".join(cells))    
+               
     # Movement
     T = input().lower()
     if (T == "left"):
