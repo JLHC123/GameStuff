d0305dd974fc5903606e3fadc50b192686eb2fe5
JLHC123 <jlhc3751@gmail.com>
2025-12-23 22:11:20 -0800

    we made all non path cells into walls just to see where we need to improve

--- PythonLearning.py
+++ PythonLearning.py
@@ -47,7 +47,7 @@ for y in range(1, n - 1):
 # random wall generation
 for y in range(1, n - 1):
     for x in range(1, n - 1):
-        if random.randint(0, 10) < 9:
+        if random.randint(0, 10) < 11:
             random_maze[y][x] = 1
 
 # valid path generation
